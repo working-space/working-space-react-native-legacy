59244b67646303897460edd38462889d3b52f59a
Shim MunSeong <Puterism.K@gmail.com>
2021-02-11 15:23:12 +0900

    chore: 지도 페이지에서 console.warn 임시 비활성화

--- src/screens/Map/Map.js
+++ src/screens/Map/Map.js
@@ -19,41 +19,45 @@ const Map = () => {
         style={{ width: '100%', height: '100%' }}
         showsMyLocationButton={true}
         center={{ ...P0, zoom: 16 }}
-        onTouch={(e) => console.warn('onTouch', JSON.stringify(e.nativeEvent))}
-        onCameraChange={(e) =>
-          console.warn('onCameraChange', JSON.stringify(e))
-        }
-        onMapClick={(e) => console.warn('onMapClick', JSON.stringify(e))}>
-        <Marker coordinate={P0} onClick={() => console.warn('onClick! p0')} />
+        // onTouch={(e) => console.warn('onTouch', JSON.stringify(e.nativeEvent))}
+        // onCameraChange={(e) =>
+        //   console.warn('onCameraChange', JSON.stringify(e))
+        // }
+        // onMapClick={(e) => console.warn('onMapClick', JSON.stringify(e))}
+      >
+        <Marker
+          coordinate={P0}
+          // onClick={() => console.warn('onClick! p0')}
+        />
         <Marker
           coordinate={P1}
           pinColor="blue"
-          onClick={() => console.warn('onClick! p1')}
+          // onClick={() => console.warn('onClick! p1')}
         />
         <Marker
           coordinate={P2}
           pinColor="red"
-          onClick={() => console.warn('onClick! p2')}
+          // onClick={() => console.warn('onClick! p2')}
         />
         <Path
           coordinates={[P0, P1]}
-          onClick={() => console.warn('onClick! path')}
+          // onClick={() => console.warn('onClick! path')}
           width={10}
         />
         <Polyline
           coordinates={[P1, P2]}
-          onClick={() => console.warn('onClick! polyline')}
+          // onClick={() => console.warn('onClick! polyline')}
         />
         <Circle
           coordinate={P0}
           color={'rgba(255,0,0,0.3)'}
           radius={200}
-          onClick={() => console.warn('onClick! circle')}
+          // onClick={() => console.warn('onClick! circle')}
         />
         <Polygon
           coordinates={[P0, P1, P2]}
           color={`rgba(0, 0, 0, 0.5)`}
-          onClick={() => console.warn('onClick! polygon')}
+          // onClick={() => console.warn('onClick! polygon')}
         />
       </NaverMapView>
     </Container>
