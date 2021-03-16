0ec2da549ce8512ba2ac98a31a0f06fd67397cb7
leejiwonn <bbongwa@naver.com>
2021-03-16 22:10:38 +0900

    feat: 'Detail' 페이지 찜, 북마크 기능 활성화 - 태그 및 댓글 등록 기능 구현중

--- src/screens/Detail/Detail.js
+++ src/screens/Detail/Detail.js
@@ -12,23 +12,25 @@ import TagList from '~/components/TagList/TagList';
 import CommentList from '~/components/CommentList/CommentList';
 import ImageGrid from '~/components/ImageGrid/ImageGrid';
 import SetTags from '~/components/SetTags/SetTags';
+import InputComment from '~/components/InputComment/InputComment';
 import BackIcon from '~/assets/icons/icon_back.svg';
 import ShareIcon from '~/assets/icons/icon_share.svg';
 import FavoriteIcon from '~/assets/icons/icon_favorite.svg';
+import FavoriteFillIcon from '~/assets/icons/icon_favorite_fill.svg';
 import BookmarkIcon from '~/assets/icons/icon_bookmark.svg';
+import BookmarkFillIcon from '~/assets/icons/icon_bookmark_fill.svg';
 import CloseIcon from '~/assets/icons/icon_close.svg';
+import { KeyboardAvoidingView } from 'react-native';
 
 const Detail = ({ like, userPreferTags, route, navigation: { goBack } }) => {
   const { cardData } = route.params;
   const { title, distance, tags, address } = cardData;
 
-  const [visibleInput, setVisibleInput] = useState(false);
-  const [preferTags, setPreferTags] = useState([]);
-  const [selectPreferTags, setSelectPreferTags] = useState([]);
-
-  useEffect(() => {
-    setPreferTags(userPreferTags);
-  }, [selectPreferTags, userPreferTags]);
+  const [visibleInput, setVisibleInput] = useState(null);
+  const [preferTags, setPreferTags] = useState([...userPreferTags]);
+  const [selectPreferTags, setSelectPreferTags] = useState([...userPreferTags]);
+  const [toggleFavorite, setToggleFavorite] = useState(false);
+  const [toggleBookmark, setToggleBookmark] = useState(false);
 
   useEffect(() => {
     console.log('preferTags', preferTags);
@@ -50,17 +52,29 @@ const Detail = ({ like, userPreferTags, route, navigation: { goBack } }) => {
   }, []);
 
   const handleModalEvaluation = useCallback(() => {
-    setVisibleInput(true);
+    setVisibleInput('Tags');
   }, []);
 
   const handleSubmitBtn = useCallback(() => {
-    setVisibleInput(false);
+    setVisibleInput(null);
     setSelectPreferTags([]);
   }, []);
 
   const handleCloseBtn = useCallback(() => {
-    setVisibleInput(false);
+    setVisibleInput(null);
     setSelectPreferTags([]);
+  }, []);
+
+  const handleToggleFavoriteBtn = useCallback(() => {
+    setToggleFavorite(toggleFavorite ? false : true);
+  }, [toggleFavorite]);
+
+  const handleToggleBookmarkBtn = useCallback(() => {
+    setToggleBookmark(toggleBookmark ? false : true);
+  }, [toggleBookmark]);
+
+  const handleSetCommentTextModal = useCallback(() => {
+    setVisibleInput('Comments');
   }, []);
 
   return (
@@ -74,13 +88,11 @@ const Detail = ({ like, userPreferTags, route, navigation: { goBack } }) => {
         }
         right={
           <LinkIconWrapper>
-            <LinkIconWrapper.Item>
-              <FavoriteIcon width="24" height="24" />
-              <LinkIconWrapper.Text>{like}</LinkIconWrapper.Text>
+            <LinkIconWrapper.Item onPress={handleToggleFavoriteBtn}>
+              {toggleFavorite ? <FavoriteFillIcon width="24" height="24" /> : <FavoriteIcon width="24" height="24" />}
+              <LinkIconWrapper.Text>{like + toggleFavorite}</LinkIconWrapper.Text>
             </LinkIconWrapper.Item>
-            <LinkIconWrapper.Item>
-              <BookmarkIcon width="24" height="24" />
-            </LinkIconWrapper.Item>
+            <LinkIconWrapper.Item onPress={handleToggleBookmarkBtn}>{toggleBookmark ? <BookmarkFillIcon width="24" height="24" /> : <BookmarkIcon width="24" height="24" />}</LinkIconWrapper.Item>
             <LinkIconWrapper.Item>
               <ShareIcon width="24" height="24" />
             </LinkIconWrapper.Item>
@@ -93,9 +105,9 @@ const Detail = ({ like, userPreferTags, route, navigation: { goBack } }) => {
         <DetailInfo address={address} />
         <DetailLocation />
         <TagList tags={tags} preferTags={preferTags} onModalEvaluation={handleModalEvaluation} />
-        <CommentList />
+        <CommentList onSetCommentTextModal={handleSetCommentTextModal} />
       </DetailWrapper>
-      <Modal style={{ width: '100%', margin: 0 }} isVisible={visibleInput} onBackButtonPress={handleCloseBtn} hideModalContentWhileAnimating={true} useNativeDriver={true}>
+      <Modal style={{ width: '100%', margin: 0 }} isVisible={visibleInput === 'Tags'} onBackButtonPress={handleCloseBtn} hideModalContentWhileAnimating={true} useNativeDriver={true}>
         <ModalEvaluation>
           <ModalEvaluation.Header>
             <ModalEvaluation.Top>
@@ -114,11 +126,22 @@ const Detail = ({ like, userPreferTags, route, navigation: { goBack } }) => {
               background-color: ${isEmpty(preferTags) && isEmpty(selectPreferTags) ? '#cccccc' : '#ffbb44'};
             `}>
             <ModalEvaluation.Text>
-              {isEmpty(preferTags) && isEmpty(selectPreferTags) ? '태그가 선택되지 않았어요.' : `태그 ${preferTags.length + selectPreferTags.length}개 선택! 평가 등록하기`}
+              {isEmpty(preferTags) && isEmpty(selectPreferTags) ? '태그가 선택되지 않았어요.' : `태그 ${selectPreferTags ? selectPreferTags.length : preferTags.length}개 선택! 평가 등록하기`}
             </ModalEvaluation.Text>
           </ModalEvaluation.SubmitButton>
         </ModalEvaluation>
       </Modal>
+      <KeyboardAvoidingView>
+        <Modal
+          style={{ width: '100%', margin: 0 }}
+          backdropOpacity={0}
+          isVisible={visibleInput === 'Comments'}
+          onBackButtonPress={handleCloseBtn}
+          hideModalContentWhileAnimating={true}
+          useNativeDriver={true}>
+          <InputComment />
+        </Modal>
+      </KeyboardAvoidingView>
     </>
   );
 };
