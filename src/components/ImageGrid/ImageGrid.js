edb5a222688d759fe98d0a991c6f98100dd06f53
leejiwonn <bbongwa@naver.com>
2021-03-30 16:30:00 +0900

    feature: 이미지 UI 구현중 - `react-native-fb-collage` 라이브러리 적용 : 상세 이미지 뷰 구현에 사용 - `react-native-slick` 라이브러리 적용 => 상세 이미지 클릭 시 Modal 내부 구현에 사용

--- src/components/ImageGrid/ImageGrid.js
+++ src/components/ImageGrid/ImageGrid.js
@@ -1,13 +1,57 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import { Text } from 'react-native';
+import FBCollage from 'react-native-fb-collage';
+import Slick from 'react-native-slick';
+import Modal from 'react-native-modal';
 import { ImageWrapper } from './ImageGrid.styles';
 
-const ImageGrid = () => {
+const ImageGrid = (props) => {
+  const { cardImages } = props;
+  const [visibleInput, setVisibleInput] = useState(null);
+  const handleCloseBtn = useCallback(() => {
+    setVisibleInput(null);
+  }, []);
+
   return (
-    <ImageWrapper>
-      <ImageWrapper.Box></ImageWrapper.Box>
-    </ImageWrapper>
+    <>
+      <ImageWrapper>
+        <FBCollage
+          images={cardImages}
+          imageOnPress={(index) => {
+            setVisibleInput(index);
+          }}
+          width={window.innerWidth}
+          height={150}
+          spacing={2}
+          borderRadius={2}
+        />
+      </ImageWrapper>
+      <Modal
+        style={{ width: '100%', margin: 0, backgroundColor: '#ffffff' }}
+        backdropOpacity={0}
+        isVisible={visibleInput !== null}
+        onBackButtonPress={handleCloseBtn}
+        hideModalContentWhileAnimating={true}
+        useNativeDriver={true}>
+        <Slick showsButtons={true}>
+          {cardImages.map((cardImage) => (
+            <Text>{cardImage}</Text>
+          ))}
+        </Slick>
+      </Modal>
+    </>
   );
+};
+
+ImageGrid.defaultProps = {
+  cardImages: [
+    'https://cdn.inflearn.com/wp-content/uploads/web3-1.png',
+    'https://cdn.inflearn.com/wp-content/uploads/web3-1.png',
+    'https://cdn.inflearn.com/wp-content/uploads/web3-1.png',
+    'https://cdn.inflearn.com/wp-content/uploads/web3-1.png',
+    'https://cdn.inflearn.com/wp-content/uploads/web3-1.png',
+    'https://cdn.inflearn.com/wp-content/uploads/web3-1.png',
+  ],
 };
 
 export default ImageGrid;
