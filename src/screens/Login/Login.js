685ae3e0b7a8e449bece28e09976373aad51cd71
leejiwonn <bbongwa@naver.com>
2021-04-29 00:38:16 +0900

    fix: 일러스트 이미지 및 css 변경

--- src/screens/Login/Login.js
+++ src/screens/Login/Login.js
@@ -3,9 +3,9 @@ import { Image } from 'react-native';
 import { login, getProfile as getKakaoProfile, KakaoOAuthToken, KakaoProfile, logout, unlink } from '@react-native-seoul/kakao-login';
 import { observer } from 'mobx-react-lite';
 import AutoFitImage from '~/components/AutoFitImage/AutoFitImage';
-import DummyLoginIllustURL from '~/assets/images/dummy_illust.jpg';
+import LoginIllustURL from '~/assets/images/login_illust.jpg';
 import GoogleButtonIcon from '~/assets/icons/google_button.svg';
-import { HeaderText, Container, Footer, LoginBtn } from './Login.styles';
+import { HeaderText, Container, Footer, LoginButton } from './Login.styles';
 
 const Login = ({ navigation }) => {
   const onLogin = async () => {
@@ -21,20 +21,20 @@ const Login = ({ navigation }) => {
   return (
     <Container>
       <HeaderText>원하는 작업 공간을{'\n'}직접 찾아보세요!</HeaderText>
-      <AutoFitImage source={DummyLoginIllustURL} />
+      <AutoFitImage source={LoginIllustURL} />
       <Footer>
-        <LoginBtn onPress={onLogin}>
-          <LoginBtn.Icon>
+        <LoginButton onPress={onLogin}>
+          <LoginButton.Icon>
             <Image style={{ width: '100%', height: '100%' }} source={require('~/assets/icons/kakao_button.png')} />
-          </LoginBtn.Icon>
-          <LoginBtn.Text>카카오 로그인</LoginBtn.Text>
-        </LoginBtn>
-        <LoginBtn>
-          <LoginBtn.Icon>
+          </LoginButton.Icon>
+          <LoginButton.Text>카카오 로그인</LoginButton.Text>
+        </LoginButton>
+        <LoginButton>
+          <LoginButton.Icon>
             <GoogleButtonIcon />
-          </LoginBtn.Icon>
-          <LoginBtn.Text>구글 로그인</LoginBtn.Text>
-        </LoginBtn>
+          </LoginButton.Icon>
+          <LoginButton.Text>구글 로그인</LoginButton.Text>
+        </LoginButton>
       </Footer>
     </Container>
   );
