f47bef862040bc4436844c521f40a556bfa52c39
Shim MunSeong <Puterism.K@gmail.com>
2021-05-15 19:51:27 +0900

    feat: 카페 상세 정보 페이지에서 지도가 보이도록 구현

--- src/components/DetailInfo/DetailLocation.js
+++ src/components/DetailInfo/DetailLocation.js
@@ -1,14 +1,36 @@
+import { css } from '@emotion/native';
 import React from 'react';
-import { Text } from 'react-native';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import MapMarker from '~/components/MapMarker/MapMarker';
 import { DetailLocationWrapper, DetailLocationBox, Title, LocationMap } from './DetailLocation.styles';
+import MapPickerSelectImage from '~/assets/icons/icon_mappicker_select.png';
 
-const DetailLocation = () => {
+const DetailLocation = ({ latitude, longitude }) => {
   return (
     <DetailLocationWrapper>
       <DetailLocationBox>
         <Title>위치</Title>
         <LocationMap>
-          <Text>Location</Text>
+          <MapView
+            cacheEnabled={true}
+            loadingEnabled={true}
+            moveOnMarkerPress={false}
+            pitchEnabled={false}
+            rotateEnabled={false}
+            zoomEnabled={false}
+            scrollEnabled={false}
+            provider={PROVIDER_GOOGLE}
+            style={css`
+              flex: 1;
+            `}
+            initialRegion={{
+              latitude: latitude,
+              longitude: longitude,
+              latitudeDelta: 0.005,
+              longitudeDelta: 0.005,
+            }}>
+            <MapMarker coordinate={{ latitude, longitude }} image={MapPickerSelectImage} tracksViewChanges={true} stopPropagation={true} />
+          </MapView>
         </LocationMap>
       </DetailLocationBox>
     </DetailLocationWrapper>
