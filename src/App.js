25750a8133083a9d7a408474e68f45b1a05028ff
Shim MunSeong <Puterism.K@gmail.com>
2021-05-27 01:42:11 +0900

    ui: 안드로이드에서 Lottie 스플래시 스크린 적용

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import SplashScreen from 'react-native-lottie-splash-screen';
 import Navigator from '~/navigations';
 import 'react-native-gesture-handler';
 
 const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return <Navigator />;
 };
 
